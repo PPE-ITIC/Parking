ef178dff2705cb1f6d4539b40f2810597b67482c
dinesoft <amandine.bresil@free.fr>
2018-09-18 06:36:22 +0200

    Site
    Ajout de dossier et de fichier pour le début du site côté front.

---
+++ layout.php
@@ -0,0 +1,69 @@
+<head>
+    <meta charset="utf-8">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <title>Pakrking ITIC</title>
+</head>
+
+<body>
+  <body data-spy="scroll" data-target=".navbar" data-offset="50">
+
+
+<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">LogoSite</a>
+    </div>
+    <div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li><a href="#section1">Le concept</a></li>
+          <li><a href="#section2">S'inscrire</a></li>
+          <li><a href="#section3">Avis</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="container-fluid banniere">
+  <h1 id="titre">Trouvez votre place de parking !</h1>
+</div>    
+
+<div id="section1" class="container-fluid">
+  <h1>Le concept</h1>
+  <p>Notre agence PakingItic vous aide à trouver votre place de parking dans Paris.</p>
+</div>
+<div id="section2" class="container-fluid">
+  <h1>S'inscrire'</h1>
+  <p>Blablabla</p>
+
+</div>
+<div id="section3" class="container-fluid">
+  <h1>Avis</h1>
+  <p>Blablabla</p>
+</div>
+
+   
+
+
+   <!-- ScrollMagic -->
+   
+   <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+   <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+   <script type="text/javascript" src="../../js/lib/greensock/plugins/ScrollToPlugin.min.js"></script>
+   <script src="js/app.js"></script>
+    
+</body>
+<footer>
+    
+</footer>
+

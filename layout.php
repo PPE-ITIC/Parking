8e38e6b5577b1c052b328454e92f3d886b500f56
dinesoft <amandine.bresil@free.fr>
2018-09-19 23:32:39 +0200

    Maj
    Continuation du site

--- layout.php
+++ layout.php
@@ -1,69 +1,85 @@
 <head>
     <meta charset="utf-8">
-    
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/scrollreveal@3.3.2/dist/scrollreveal.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lato:700|Noto+Serif" rel="stylesheet">
 
     <title>Pakrking ITIC</title>
 </head>
 
 <body>
-  <body data-spy="scroll" data-target=".navbar" data-offset="50">
+
+    <body data-spy="scroll" data-target=".navbar" data-offset="50">
 
 
-<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
-  <div class="container-fluid">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">LogoSite</a>
-    </div>
-    <div>
-      <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav">
-          <li><a href="#section1">Le concept</a></li>
-          <li><a href="#section2">S'inscrire</a></li>
-          <li><a href="#section3">Avis</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</nav>
-<div class="container-fluid banniere">
-  <h1 id="titre">Trouvez votre place de parking !</h1>
-</div>    
+                    <a class="navbar-brand" href="#">LogoSite</a>
+                </div>
+                <div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#section1">Le concept</a></li>
+                            <li><a href="#section2">S'inscrire</a></li>
+                            <li><a href="#section3">Avis</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid banniere">
+            <h1 id="titre" class="titre titreAnimate"><b>Votre place de parking<br>à porté de main</b></h1>
+            <br>
+            <input type="button" class="btn btnAnimate" value="C'est parti !">
+        </div>
 
-<div id="section1" class="container-fluid">
-  <h1>Le concept</h1>
-  <p>Notre agence PakingItic vous aide à trouver votre place de parking dans Paris.</p>
-</div>
-<div id="section2" class="container-fluid">
-  <h1>S'inscrire'</h1>
-  <p>Blablabla</p>
+        <div id="section1" class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Le concept</h1>
+                    <p>Votre place de parking à porté de main !</p>
+                    <br>
+                    <p>
+                    Nous prenons en charge votre stationnement en vous trouvant la place de parking gratuite la plus proche de votre destination !
+                    </p>
+                
+                </div>
+                <div class="col-md-6 imgAnimate">
+                    <img src="images/voiture.jpg" style="height:250px;width:500px;">
+                </div>
+            </div>
+        </div>
+        <div id="section2" class="container-fluid">
+            <h1>N'attendez plus !</h1>
+            <p>Ne perdez plus de temps à tourner autour d'un même quartier en attendant qu'une place se libère, et rejoignez ParkingItic ! </p>
 
-</div>
-<div id="section3" class="container-fluid">
-  <h1>Avis</h1>
-  <p>Blablabla</p>
-</div>
-
-   
+        </div>
+        <div id="section3" class="container-fluid">
+            <h1>Avis</h1>
+            <p>Blablabla</p>
+        </div>
 
 
-   <!-- ScrollMagic -->
-   
-   <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
-   <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
-   <script type="text/javascript" src="../../js/lib/greensock/plugins/ScrollToPlugin.min.js"></script>
-   <script src="js/app.js"></script>
-    
-</body>
-<footer>
-    
-</footer>
 
+
+        <!-- ScrollMagic -->
+
+        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+        <script type="text/javascript" src="../../js/lib/greensock/plugins/ScrollToPlugin.min.js"></script>
+        <script src="js/app.js"></script>
+
+    </body>
+    <footer>
+
+    </footer>

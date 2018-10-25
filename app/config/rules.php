f74bda643c02b51733c00a4cc834e0aa2738028b
dinesoft <amandine.bresil@free.fr>
2018-10-26 00:57:13 +0200

    Ajout de l'action déconnexion aux rules

--- app/config/rules.php
+++ app/config/rules.php
@@ -7,11 +7,14 @@ if (!isset($_SESSION['router']))
     $router->addRule('register', array('controller' => 'personne', 'action' => 'register'));
     $router->addRule('inscription', array('controller' => 'personne', 'action' => 'inscription'));
     $router->addRule('connexion', array('controller' => 'personne', 'action' => 'connexion'));
+    $router->addRule('deconnexion', array('controller' => 'personne', 'action' => 'deconnexion'));
     $router->addRule('admin', array('controller' => 'admin', 'action' => 'index'));
     $router->addRule('user', array('controller' => 'personne', 'action' => 'index'));
-    $router->addRule('deconnexion', array('controller' => 'personne', 'action' => 'deconnexion'));
     $router->addRule('valider/:id', array('controller' => 'admin', 'action' => 'valider'));
     $router->addRule('refuser/:id', array('controller' => 'admin', 'action' => 'refuser'));
+    $router->addRule('monter/:id', array('controller' => 'admin', 'action' => 'monter'));
+    $router->addRule('descendre/:id', array('controller' => 'admin', 'action' => 'descendre'));
+    $router->addRule('attribuer/:id', array('controller' => 'admin', 'action' => 'attribuer'));
 }
 else
 {

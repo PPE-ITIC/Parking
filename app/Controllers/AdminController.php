a989b4b38c1f6a2b737e13c1452f16b8886a9236
dinesoft <amandine.bresil@free.fr>
2018-10-26 00:59:30 +0200

    Ajout des fonction monter et descendre

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -18,6 +18,10 @@ class AdminController extends BaseController
                 $crudPersonne = new Parking\DbTable\Personne($this->connection);
                 $personnes = $crudPersonne->findByStatut(INV_ID);
                 $data['personnes'] = $personnes;
+                $users = $crudPersonne->findAll();
+                $data['users'] = $users;
+                $attentes = $crudPersonne->findByStatut(IA_ID);
+                $data['attentes'] = $attentes;
                 $this->setView('adminView');
                 $this->render($data);
             }
@@ -54,6 +58,7 @@ class AdminController extends BaseController
                     $persUpd->setStatut($statut);
                     $crudPersonne->update($persUpd);
                 }
+                $_SESSION['onglet'] = "inscriptions";
                 header("Location: ".BASE_URL."admin");
                 exit;
             }
@@ -90,6 +95,91 @@ class AdminController extends BaseController
                     $persUpd->setStatut($statut);
                     $crudPersonne->update($persUpd);
                 }
+                $_SESSION['onglet'] = "inscriptions";
+                header("Location: ".BASE_URL."admin");
+                exit;
+            }
+            else
+            {
+                $_SESSION['connexion_error'] = "Vous devez vous connecter à votre compte admin pour accéder à cette page";
+            }
+        }
+        else
+        {
+            $_SESSION['connexion_error'] = "Vous devez vous connecter pour accéder à cette page";
+        }
+        if (isset($_SESSION['connexion_error']))
+        {
+            $this->setView('loginView');
+            $this->render();
+        }
+    }
+
+    public function monterAction($params)
+    {
+        if (isset($_SESSION['personne']) && is_object (unserialize($_SESSION['personne'])) && get_class(unserialize($_SESSION['personne'])) == 'Parking\Personne')
+        {
+            $personne = unserialize($_SESSION['personne']);
+            if ($personne->isAdmin())
+            {
+                $crudPersonne = new Parking\DbTable\Personne($this->connection);
+                $persUpd = $crudPersonne->find($params['id']);
+
+                if ($persUpd)
+                {
+                    $ordre = $persUpd->getAttente()->getOrdre() - 1;
+                    $ordre_replace = $persUpd->getAttente()->getOrdre();
+                    $crudAttente = new Parking\DbTable\Attente($this->connection);
+                    $attente = $crudAttente->findByPersonne($persUpd->getId());
+                    $attente->setOrdre($ordre);
+                    $attente_replace = $crudAttente->findByOrdre($ordre);
+                    $attente_replace->setOrdre($ordre_replace);
+                    $crudAttente->update($attente);
+                    $crudAttente->update($attente_replace);
+                }
+                $_SESSION['onglet'] = "liste_attente";
+                header("Location: ".BASE_URL."admin");
+                exit;
+            }
+            else
+            {
+                $_SESSION['connexion_error'] = "Vous devez vous connecter à votre compte admin pour accéder à cette page";
+            }
+        }
+        else
+        {
+            $_SESSION['connexion_error'] = "Vous devez vous connecter pour accéder à cette page";
+        }
+        if (isset($_SESSION['connexion_error']))
+        {
+            $this->setView('loginView');
+            $this->render();
+        }
+    }
+
+    public function descendreAction($params)
+    {
+        if (isset($_SESSION['personne']) && is_object (unserialize($_SESSION['personne'])) && get_class(unserialize($_SESSION['personne'])) == 'Parking\Personne')
+        {
+            $personne = unserialize($_SESSION['personne']);
+            if ($personne->isAdmin())
+            {
+                $crudPersonne = new Parking\DbTable\Personne($this->connection);
+                $persUpd = $crudPersonne->find($params['id']);
+
+                if ($persUpd)
+                {
+                    $ordre = $persUpd->getAttente()->getOrdre() + 1;
+                    $ordre_replace = $persUpd->getAttente()->getOrdre();
+                    $crudAttente = new Parking\DbTable\Attente($this->connection);
+                    $attente = $crudAttente->findByPersonne($persUpd->getId());
+                    $attente->setOrdre($ordre);
+                    $attente_replace = $crudAttente->findByOrdre($ordre);
+                    $attente_replace->setOrdre($ordre_replace);
+                    $crudAttente->update($attente);
+                    $crudAttente->update($attente_replace);
+                }
+                $_SESSION['onglet'] = "liste_attente";
                 header("Location: ".BASE_URL."admin");
                 exit;
             }

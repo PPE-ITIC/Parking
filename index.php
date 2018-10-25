b7c0305aa6a913c61cd4de687ce70b7611c275e1
dinesoft <amandine.bresil@free.fr>
2018-10-26 00:26:56 +0200

    mise à jour

--- index.php
+++ index.php
@@ -1,31 +1,4 @@
 <?php
-
-    session_start();
-
-    try{
-        $bdd = new PDO('mysql:host=localhost;dbname=parking', 'root','root');
-    }catch(Exception $e)
-    {
-        die("BDD non trouvée");
-    }
-
-    if(!isset($_GET['p']))
-    {
-        $page = 'accueil';
-    }
-    else
-    {
-        if(!file_exists("controllers/".$_GET['p']."Controller.php"))
-            $page="404";
-        else
-            $page = $_GET['p'];
-    }
-
-    ob_start();
-        include "controllers/".$page."Controller.php";
-        $content = ob_get_contents();
-    ob_end_clean();
-
-    include "layout.php";
-
-?>
+require_once 'app/init.php';
+$router->load();
+$_SESSION['router'] = serialize($router);
